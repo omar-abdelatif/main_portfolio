d79fd79703ac04950cfbb5fd7cb12f3205cf4ce9
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2025-05-05 19:13:58 +0300

    update open graph data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import Layout from '@/components/layout';
 import Hero from '@/components/hero';
+import Head from 'next/head';
+
 
 export default function Home() {
     return (
-        <Layout>
-            <Hero />
-        </Layout>
+        <>
+            <Head>
+                <meta property="og:url" content='https://omarabdelatif.vercel.app/' />
+                <meta property="og:image" content='/assets/images/logo.png' />
+                <meta property="og:title" content='Omar Abdelatif' />
+                <meta property="og:description" content='Egyptian Full Stack Developer With laravel and Next.js' />
+                <meta name="twitter:title" content="Omar Abdelatif" />
+                <meta name="twitter:description" content="Egyptian Full Stack Developer With laravel and Next.js" />
+                <meta name="twitter:image" content="/assets/images/logo.png" />
+            </Head>
+            <Layout>
+                <Hero />
+            </Layout>
+        </>
     );
 }
