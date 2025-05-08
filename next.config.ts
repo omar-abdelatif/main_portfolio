66f8d8ccda1fccb9a12ff76fcb9a0bee76b5cc1a
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2025-05-08 23:18:23 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ['portfolio.adendan.com'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'portfolio.adendan.com',
       },
       {
         protocol: 'http',
-        hostname: '**',
+        hostname: 'portfolio.adendan.com',
       },
     ],
   },
   env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
   },
   reactStrictMode: true,
